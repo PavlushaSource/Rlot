20692cf5210fe00c1c63467426ea3d2cc29ee100
PavlushaSource <pavlushkin.dima1785@gmail.com>
2024-09-19 01:57:58 +0300

    refactor: Change env prepare for mdadm

--- lib/logger/mdadm_logger.py
+++ lib/logger/mdadm_logger.py
@@ -11,7 +11,7 @@ class Mdadm_logger(Logger, ABC):
 
 
     def _get_file_name_param(self) -> str:
-        return f"MDADM_{self.settings['raid']['number_realization']}"
+        return f"dev/md_{self.settings['raid']['number_realization']}"
 
     def _get_mode(self) -> str:
         return "mdadm"
@@ -22,7 +22,7 @@ class Mdadm_logger(Logger, ABC):
         command_init.append("--verbose")
         command_init.append("--chunk=512")
         command_init.append(self._get_file_name_param())
-        command_init.append(self.settings["raid"]["number_realization"])
+        command_init.append(f"--level={self.settings["raid"]["number_realization"]}")
 
         command_init.append(f"--raid-devices={len(self.__devices)}")
         for x in self.__devices:
@@ -30,8 +30,10 @@ class Mdadm_logger(Logger, ABC):
 
         return command_init
 
-    def __start_logger(self) -> None:
+    def start_logger(self) -> None:
+    
         command = self.__create_mdadm_init_command()
+        print("Start logger", command)
         run_command(command)
 
         self.settings['raid']['dev'] = self._get_file_name_param()
