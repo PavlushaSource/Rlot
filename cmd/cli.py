58df7eca43ebf0510695a668aef4ff29319b95c6
PavlushaSource <pavlushkin.dima1785@gmail.com>
2024-09-19 01:43:40 +0300

    feat: Add ABC class

--- cmd/cli.py
+++ cmd/cli.py
@@ -2,13 +2,14 @@ import sys
 
 from lib import utils
 from lib.arg_parser import parser
-from lib.logger.bdev_logger import bdev_logger
+from lib.logger.mdadm_logger import Mdadm_logger
+from lib.logger.bdev_logger import Bdev_logger
 
 def main():
     utils.check_fio_exists()
     config = parser.get_config()
 
-    logger = bdev_logger(settings=config)
+    logger = Mdadm_logger(settings=config)
     logger.generate_fio_file()
     logger.run_fio()
 
