fe83344dc6bf2a7541ea094d78dcbd440ba12dbd
PavlushaSource <pavlushkin.dima1785@gmail.com>
2024-09-19 01:43:40 +0300

    feat: Add ABC class

---
+++ lib/logger/logger.py
@@ -0,0 +1,80 @@
+from abc import ABC, abstractmethod
+from configparser import ConfigParser
+import os
+import sys
+from lib.utils import define_mode_dev, get_current_data, get_root_path, run_command
+
+
+class Logger(ABC):
+
+    @abstractmethod
+    def _get_file_name_param(self) -> str: ...
+
+    @abstractmethod
+    def _get_mode(self) -> str: ...
+
+    def __init__(self, settings: ConfigParser) -> None:
+        self.settings = settings
+        self.__fio_file_path = None
+        self.__logs_dir_path = None
+        self.__start_logger()
+
+    def __start_logger(self) -> None:
+        pass
+
+    def free_logger(self) -> None:
+        pass
+
+    def __create_config_for_fio(self) -> ConfigParser:
+        fio_file = ConfigParser(allow_no_value=True)
+        file_name_param = self._get_file_name_param()
+
+        fio_file["global"] = {}
+        fio_file.set("global", "time_based", None)
+        for key, value in self.settings.items("global"):
+            if key not in ["rw", "dev"]:
+                fio_file["global"][key] = value
+
+        for rw in [i.strip() for i in self.settings["global"]["rw"].split(",")]:
+            section_name = f"{rw}-{self.settings['global']['bs']}"
+            fio_file[section_name] = {}
+            fio_file[section_name]["filename"] = file_name_param
+            fio_file[section_name]["rw"] = rw
+
+            # TODO think about log_avg_msec
+            fio_file[section_name]["log_avg_msec"] = "1000"
+
+            mode = self._get_mode()
+            fio_file[section_name][
+                "write_bw_log"
+            ] = f"{self.__logs_dir_path}/{self.settings['global']['bs']}-{rw}-{mode}.results"
+            fio_file[section_name][
+                "write_iops_log"
+            ] = f"{self.__logs_dir_path}/{self.settings['global']['bs']}-{rw}-{mode}.results"
+            fio_file[section_name][
+                "write_lat_log"
+            ] = f"{self.__logs_dir_path}/{self.settings['global']['bs']}-{rw}-{mode}.results"
+
+        return fio_file
+
+    def __write_fio_to_file(self, fio_config: ConfigParser) -> None:
+        try:
+            with open(self.__fio_file_path, "w") as fio_file:
+                fio_config.write(fio_file, space_around_delimiters=False)
+        except IOError:
+            print(f"Failed to write temporary Fio job file at tmpjobfile")
+            sys.exit(3)
+
+    def generate_fio_file(self) -> None:
+        current_time = get_current_data()
+        root_path = get_root_path()
+        self.__fio_file_path = f"{root_path}/tmp/tmpfile-{current_time}.fio"
+        self.__logs_dir_path = f"{root_path}/tmp/logs-dir-{current_time}"
+        os.makedirs(self.__logs_dir_path, exist_ok=True)
+
+        fio_config = self.__create_config_for_fio()
+        self.__write_fio_to_file(fio_config)
+
+    def run_fio(self) -> None:
+        command = ["fio", self.__fio_file_path]
+        run_command(command)
--- lib/logger/mdadm_logger.py
+++ lib/logger/mdadm_logger.py
@@ -1,19 +1,19 @@
 from abc import ABC
 from configparser import ConfigParser
-from bdev_logger import Bdev_logger
+from .logger import Logger
 
 from lib.utils import run_command
 
-class mdadm_logger(Bdev_logger, ABC):
+class Mdadm_logger(Logger, ABC):
     def __init__(self, settings: ConfigParser) -> None:
-        self.__devices = [i.strip() for i in self.settings["raid"]["dev"].split(",")]
+        self.__devices = [i.strip() for i in settings["raid"]["dev"].split(",")]
         super().__init__(settings)
 
 
-    def __get_file_name_param(self) -> str:
+    def _get_file_name_param(self) -> str:
         return f"MDADM_{self.settings['raid']['number_realization']}"
 
-    def __get_mode() -> str:
+    def _get_mode(self) -> str:
         return "mdadm"
 
     def __create_mdadm_init_command(self) -> list[str]:
@@ -21,7 +21,7 @@ class mdadm_logger(Bdev_logger, ABC):
         command_init.append("--create")
         command_init.append("--verbose")
         command_init.append("--chunk=512")
-        command_init.append(self.__get_file_name_param())
+        command_init.append(self._get_file_name_param())
         command_init.append(self.settings["raid"]["number_realization"])
 
         command_init.append(f"--raid-devices={len(self.__devices)}")
@@ -34,13 +34,13 @@ class mdadm_logger(Bdev_logger, ABC):
         command = self.__create_mdadm_init_command()
         run_command(command)
 
-        self.settings['raid']['dev'] = self.__get_file_name_param()
+        self.settings['raid']['dev'] = self._get_file_name_param()
 
 
     def free_logger(self) -> None:
         command = ["mdadm"]
         command.append("--stop")
-        command.append(self.__get_file_name_param())
+        command.append(self._get_file_name_param())
         run_command(command)
 
         command = ["mdadm", "--zero-superblock"]
